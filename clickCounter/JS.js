f88f9e3c42db486c7ed709d0038df816226928d5
charliechaf <chafincharlie11@gmail.com>
2018-12-10 21:52:44 -0600

    testing out the touch counter

--- clickCounter/JS.js
+++ clickCounter/JS.js
@@ -1,25 +1,31 @@
-counter = 0
-$("#clickBox").mousedown(function(){
-    counter = counter +1
-    $(".numberDisplay").html(counter)
-});
+//counter = 0
+//$("#clickBox").mousedown(function(){
+//    counter = counter +1
+//    $(".numberDisplay").html(counter)
+//});
+//
+//
+//
+//$("#clickBox").mouseup(function(){
+//    counter = counter -1
+//    $(".numberDisplay").html(counter)
+//});
+//
+//
+//$("#clickBox").ontouchstart(function(){
+//    counter = counter +1
+//    $(".numberDisplay").html(counter)
+//});
+//
+//
+//
+//$("#clickBox").ontouchend(function(){
+//    counter = counter -1
+//    $(".numberDisplay").html(counter)
+//});
 
+function countTouches(event) {
+  var x = event.touches.length;
+  $(".numberDisplay").html(x);
 
-
-$("#clickBox").mouseup(function(){
-    counter = counter -1
-    $(".numberDisplay").html(counter)
-});
-
-
-$("#clickBox").ontouchstart(function(){
-    counter = counter +1
-    $(".numberDisplay").html(counter)
-});
-
-
-
-$("#clickBox").ontouchend(function(){
-    counter = counter -1
-    $(".numberDisplay").html(counter)
-});
+}
