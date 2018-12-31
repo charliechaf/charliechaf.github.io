ef5b9dae2d611e004f0549edf594e57874097c58
charliechaf <chafincharlie11@gmail.com>
2018-12-31 12:32:43 -0600

    reformatted the json and a bunch of other javascript changes

--- epl/scatter.js
+++ epl/scatter.js
@@ -1,35 +1,37 @@
 var ctx = document.getElementById("scatterChart").getContext("2d");
 var canvas = document.getElementById('scatterChart');
-canvas.width  = 800;
-canvas.height = 600;
 
-var scatterChart = new Chart(ctx, {
-    type: 'scatter',
-    data: {
-        datasets: [{
-            label: 'Scatter Dataset',
-            data: [{
-                x: -10,
-                y: 0
-            }, {
-                x: 0,
-                y: 10
-            }, {
-                x: 10,
-                y: 5
-            }]
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            xAxes: [{
-                type: 'linear',
-                position: 'bottom'
-            }]
-        }
-    }
+var average_data = $.getJSON('epl/data/team_averages.json', function(data) {
+    console.log('data',data);
 });
 
+console.log(average_data[0].team_averages);
+
+Chart.defaults.global.defaultFontFamily = "Lato";
+Chart.defaults.global.defaultFontSize = 18;
+
+var bubbleChart = new Chart(ctx, {
+  type: 'bubble',
+  data: average_data
+});
+
+
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+
+            }
+        }
+    }
+    rawFile.send(null);
+}
 
