c4535b412377506f49c9c782709a4a42d21d14ea
hoffmann19 <hoffmann19@gmail.com>
2018-12-31 10:43:43 -0600

    uploading csv

---
+++ epl/scatter.js
@@ -0,0 +1,33 @@
+var ctx = document.getElementById("scatterChart").getContext("2d");
+var canvas = document.getElementById('scatterChart');
+canvas.width  = 800;
+canvas.height = 600;
+
+var scatterChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Scatter Dataset',
+            data: [{
+                x: -10,
+                y: 0
+            }, {
+                x: 0,
+                y: 10
+            }, {
+                x: 10,
+                y: 5
+            }]
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    }
+});
+
+
