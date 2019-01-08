bae489c69e238a9e911a4531f8b01008dc13aeeb
charliechaf <chafincharlie11@gmail.com>
2019-01-07 23:37:06 -0600

    we have a working bubble chart!! sweet.

--- epl/scatter.js
+++ epl/scatter.js
@@ -1,37 +1,279 @@
+data = [{
+       	"team": "ARS",
+       	"avg_poss": 0.59,
+       	"avg_goals": 2.1,
+       	"avg_shots": 12.4,
+       	"avg_shots_on_goal": 4.8,
+       	"avg_crosses": 22,
+       	"avg_passes": 504,
+       	"avg_passes_per_goal": 240
+       }, {
+       	"team": "BHA",
+       	"avg_poss": 0.43,
+       	"avg_goals": 1.1,
+       	"avg_shots": 9.1,
+       	"avg_shots_on_goal": 2.8,
+       	"avg_crosses": 18,
+       	"avg_passes": 288,
+       	"avg_passes_per_goal": 262
+       }, {
+       	"team": "BOU",
+       	"avg_poss": 0.47,
+       	"avg_goals": 1.4,
+       	"avg_shots": 11.4,
+       	"avg_shots_on_goal": 3.9,
+       	"avg_crosses": 24,
+       	"avg_passes": 359,
+       	"avg_passes_per_goal": 257
+       }, {
+       	"team": "BUR",
+       	"avg_poss": 0.42,
+       	"avg_goals": 1,
+       	"avg_shots": 8.9,
+       	"avg_shots_on_goal": 2.8,
+       	"avg_crosses": 21,
+       	"avg_passes": 270,
+       	"avg_passes_per_goal": 285
+       }, {
+       	"team": "CHE",
+       	"avg_poss": 0.61,
+       	"avg_goals": 1.9,
+       	"avg_shots": 16,
+       	"avg_shots_on_goal": 5.3,
+       	"avg_crosses": 22,
+       	"avg_passes": 632,
+       	"avg_passes_per_goal": 333
+       }, {
+       	"team": "CRY",
+       	"avg_poss": 0.47,
+       	"avg_goals": 0.8,
+       	"avg_shots": 12.5,
+       	"avg_shots_on_goal": 3.7,
+       	"avg_crosses": 21,
+       	"avg_passes": 354,
+       	"avg_passes_per_goal": 416
+       }, {
+       	"team": "EVE",
+       	"avg_poss": 0.51,
+       	"avg_goals": 1.6,
+       	"avg_shots": 12.8,
+       	"avg_shots_on_goal": 4.5,
+       	"avg_crosses": 26,
+       	"avg_passes": 361,
+       	"avg_passes_per_goal": 233
+       }, {
+       	"team": "FUL",
+       	"avg_poss": 0.47,
+       	"avg_goals": 0.9,
+       	"avg_shots": 12.3,
+       	"avg_shots_on_goal": 4,
+       	"avg_crosses": 18,
+       	"avg_passes": 372,
+       	"avg_passes_per_goal": 414
+       }, {
+       	"team": "HUD",
+       	"avg_poss": 0.5,
+       	"avg_goals": 0.6,
+       	"avg_shots": 11.4,
+       	"avg_shots_on_goal": 3.4,
+       	"avg_crosses": 29,
+       	"avg_passes": 354,
+       	"avg_passes_per_goal": 590
+       }, {
+       	"team": "LEI",
+       	"avg_poss": 0.5,
+       	"avg_goals": 1.2,
+       	"avg_shots": 12,
+       	"avg_shots_on_goal": 4.5,
+       	"avg_crosses": 25,
+       	"avg_passes": 363,
+       	"avg_passes_per_goal": 303
+       }, {
+       	"team": "LIV",
+       	"avg_poss": 0.58,
+       	"avg_goals": 2.4,
+       	"avg_shots": 15.7,
+       	"avg_shots_on_goal": 6.5,
+       	"avg_crosses": 24,
+       	"avg_passes": 546,
+       	"avg_passes_per_goal": 227
+       }, {
+       	"team": "MCI",
+       	"avg_poss": 0.65,
+       	"avg_goals": 2.7,
+       	"avg_shots": 17.8,
+       	"avg_shots_on_goal": 6.8,
+       	"avg_crosses": 30,
+       	"avg_passes": 652,
+       	"avg_passes_per_goal": 241
+       }, {
+       	"team": "MUN",
+       	"avg_poss": 0.53,
+       	"avg_goals": 2,
+       	"avg_shots": 13,
+       	"avg_shots_on_goal": 6.5,
+       	"avg_crosses": 24,
+       	"avg_passes": 451,
+       	"avg_passes_per_goal": 220
+       }, {
+       	"team": "NEW",
+       	"avg_poss": 0.42,
+       	"avg_goals": 0.8,
+       	"avg_shots": 10.5,
+       	"avg_shots_on_goal": 3.2,
+       	"avg_crosses": 23,
+       	"avg_passes": 265,
+       	"avg_passes_per_goal": 353
+       }, {
+       	"team": "SOU",
+       	"avg_poss": 0.46,
+       	"avg_goals": 1.1,
+       	"avg_shots": 13.8,
+       	"avg_shots_on_goal": 4.8,
+       	"avg_crosses": 22,
+       	"avg_passes": 327,
+       	"avg_passes_per_goal": 311
+       }, {
+       	"team": "TOT",
+       	"avg_poss": 0.55,
+       	"avg_goals": 2.1,
+       	"avg_shots": 12.7,
+       	"avg_shots_on_goal": 5.2,
+       	"avg_crosses": 20,
+       	"avg_passes": 470,
+       	"avg_passes_per_goal": 219
+       }, {
+       	"team": "WAT",
+       	"avg_poss": 0.48,
+       	"avg_goals": 1.4,
+       	"avg_shots": 11.9,
+       	"avg_shots_on_goal": 4.2,
+       	"avg_crosses": 22,
+       	"avg_passes": 328,
+       	"avg_passes_per_goal": 243
+       }, {
+       	"team": "WHU",
+       	"avg_poss": 0.49,
+       	"avg_goals": 1.4,
+       	"avg_shots": 11.8,
+       	"avg_shots_on_goal": 4.5,
+       	"avg_crosses": 22,
+       	"avg_passes": 349,
+       	"avg_passes_per_goal": 259
+       }, {
+       	"team": "WLV",
+       	"avg_poss": 0.47,
+       	"avg_goals": 1.1,
+       	"avg_shots": 13.2,
+       	"avg_shots_on_goal": 4.5,
+       	"avg_crosses": 23,
+       	"avg_passes": 364,
+       	"avg_passes_per_goal": 316
+       }, {
+       	"team": "yCC",
+       	"avg_poss": 0.39,
+       	"avg_goals": 1,
+       	"avg_shots": 11.2,
+       	"avg_shots_on_goal": 3.1,
+       	"avg_crosses": 23,
+       	"avg_passes": 193,
+       	"avg_passes_per_goal": 203
+       }];
+
 var ctx = document.getElementById("scatterChart").getContext("2d");
 var canvas = document.getElementById('scatterChart');
 
-var average_data = $.getJSON('epl/data/team_averages.json', function(data) {
-    console.log('data',data);
-});
 
-console.log(average_data[0].team_averages);
+var jsonStr = JSON.stringify(data);
+
+$("#shape").HTML = jsonStr;
+
+var ARS = jsonPath(data, '$.[0]');
+
+var test = jsonPath(data,'$.[19].avg_shots')[0];
+
+var teamArray = [];
+
+var team = [];
+for (var i=0; i<20; i++) {
+    team[i] = {
+        x: jsonPath(data,`$.[${i}].avg_shots`)[0],
+        y: jsonPath(data,`$.[${i}].avg_passes`)[0],
+        r: 20,
+        team: jsonPath(data,`$.[${i}].team`)[0],
+        ppg: jsonPath(data,`$.[${i}].avg_passes_per_goal`)[0]
+    };
+
+    teamArray.push(jsonPath(data,`$.[${i}].team`)[0])
+}
+
+console.log(team);
+console.log(teamArray);
 
 Chart.defaults.global.defaultFontFamily = "Lato";
 Chart.defaults.global.defaultFontSize = 18;
+Chart.Tooltip.title = teamArray
 
 var bubbleChart = new Chart(ctx, {
   type: 'bubble',
-  data: average_data
-});
+  data: {
+  labels: teamArray,
+    datasets: [{
+        label: "EPL Data",
+        backgroundColor: [
 
-
-
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-
+        					'rgb(128, 0, 0)',
+        					'rgb(170, 110, 40)',
+        					'rgb(128, 128, 0)',
+        					'rgb(0, 128, 128)',
+                            'rgb(0, 0, 128)',
+                            'rgb(0, 0, 0)',
+                            'rgb(230, 25, 75)',
+                            'rgb(245, 130, 48)',
+                            'rgb(255, 225, 25)',
+                            'rgb(210, 245, 60)',
+                            'rgb(60, 180, 75)',
+                            'rgb(70, 240, 240)',
+                            'rgb(0, 130, 200)',
+                            'rgb(145, 30, 180)',
+                            'rgb(240, 50, 230)',
+                            'rgb(128, 128, 128)',
+                            'rgb(250, 190, 190)',
+                            'rgb(255, 215, 180)',
+                            'rgb(170, 255, 195)',
+                            'rgb(230, 190, 255)'
+                        ],
+        data: team
+        }]
+    },
+    options : {
+        tooltips: {
+              callbacks: {
+                title: function(tooltipItem, data) {
+                  return data['labels'][tooltipItem[0]['index']];
+                  },
+                label: function(tooltipItem, data) {
+                  return 'Avg Shots: ' + data['datasets'][0]['data'][tooltipItem['index']]['x'];
+                 },
+                afterLabel: function(tooltipItem, data) {
+                    return 'Avg Passes: ' + data['datasets'][0]['data'][tooltipItem['index']]['y'];
+                }
+                 }
+                },
+          scales: {
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: 'Avg Passes'
+              }
+            }],
+            xAxes: [{
+            scaleLabel: {
+            display: true,
+            labelString: 'Avg Shots'
             }
+            }]
+
         }
     }
-    rawFile.send(null);
-}
-
+});
